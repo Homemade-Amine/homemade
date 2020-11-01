8ae0c85039ed217ea586eaffa7f5afd7b46ed423
Homemade-Amine <homemade.dev.amine@gmail.com>
2020-11-01 13:14:40 +0100

    First commit

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase';
+import '@firebase/auth';
+import '@firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCOJM2c71LhZJaLVAPbyb8JUw3gpR25XWM',
+  authDomain: 'homemade-9ceea.firebaseapp.com',
+  databaseURL: 'https://homemade-9ceea.firebaseio.com/',
+  projectId: 'homemade-9ceea',
+  storageBucket: 'homemade-9ceea.appspot.com',
+  messagingSenderId: '504216573446',
+  appId: '1:504216573446:ios:e511910a1de304c3f45587',
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export { firebase };
